f05f15553c080b3af8fc66b053e20f3abdeec1a2
Maooer <2847544066@qq.com>
2025-09-04 12:55:46 +0800

    Auth.vue背景改为流动

--- frontend/src/views/Auth/Auth.vue
+++ frontend/src/views/Auth/Auth.vue
@@ -12,11 +12,21 @@
 .box-auth {
   height: 100%;
   background: radial-gradient(
-    100% 193.51% at 100% 0,
+    180% 180% at 50% 50%,
     #EDF4F8 0%, #EFF2FA 16.92%,
     #FAEFF6 34.8%, #FAE6F2 48.8%,
     #FAF0F7 63.79%, #F1F1FB 81.34%,
     #F0F4F8 100%
   );
+  animation: flow 10s linear infinite alternate;
+}
+
+@keyframes flow {
+  0% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: -2000px -2000px;
+  }
 }
 </style>
