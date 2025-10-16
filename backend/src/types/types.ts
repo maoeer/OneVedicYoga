ba27e8df8ac202d071eb6f131bb9fbd0df5eb1a4
maoeer <89956405+maoeer@users.noreply.github.com>
2025-10-16 10:44:19 +0800

    Update types.ts

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -1,1 +1,13 @@
+// 定义状态码枚举
+export enum ResponseCode {
+  SUCCESS = 200, // 成功
+  BAD_REQUEST = 400, // 客户端参数错误
+  SERVER_ERROR = 500, // 服务器错误
+}
 
+// 基础响应类型
+export interface ApiResponse<T = unknown> {
+  code?: ResponseCode;
+  message?: string;
+  data?: T;
+}
