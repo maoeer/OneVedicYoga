d3901c5a4cd8599340988852e3ac82655de3273e
maoeer <89956405+maoeer@users.noreply.github.com>
2025-10-16 10:42:04 +0800

    修改了原本的响应体类型

--- backend/src/types/TypeUsers.ts
+++ backend/src/types/TypeUsers.ts
@@ -27,10 +27,18 @@ export interface RegisterReq extends Request {
   }
 }
 
+// 定义状态码枚举
+export enum ResponseCode {
+  SUCCESS = 200, // 成功
+  BAD_REQUEST = 400, // 客户端参数错误
+  SERVER_ERROR = 500, // 服务器错误
+}
+
 // 基础响应类型
-export interface LoginResponse {
+export interface ApiResponse<T = unknown> {
+  code?: ResponseCode;
   message?: string;
-  user?: Omit<User, 'password'>; // 排除密码字段
+  data?: T;
 }
 
 
